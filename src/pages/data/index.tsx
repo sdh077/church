154593665e47696522647495acb539e9c2124996
shindaeho <sdh077@naver.com>
2023-01-16 20:14:55 +0900

    update

--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -4,7 +4,7 @@ const index = () => {
   return (
     <div className="container">
       ?????
-      <div className="row">
+      {/* <div className="row">
         <div className="col-lg-3 mb-9 mb-lg-0 position-sticky top-0 pt-5">
           <a href="#!" className="p-3 hover-lift hover-shadow rounded-3 border mb-3 d-block text-dark position-relative">
             <div className="d-flex justify-content-start w-100 mb-2 align-items-center">
@@ -19,7 +19,7 @@ const index = () => {
           </a>
         </div>
         <div className="col-lg-9">nostrud exercitation</div>
-      </div>
+      </div> */}
     </div>
   )
 }
